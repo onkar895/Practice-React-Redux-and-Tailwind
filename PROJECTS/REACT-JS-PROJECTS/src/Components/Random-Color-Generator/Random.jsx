11fdb5e608848e22a573df989c45ad1378bc918c
Onkar Karale <onkarkarale4@gmail.com>
2024-08-09 00:00:16 +0530

    React Practice for responsive Navigation Bars and 25 freecodeCamp Projects

--- PROJECTS/REACT-JS-PROJECTS/src/Components/Random-Color-Generator/Random.jsx
+++ PROJECTS/REACT-JS-PROJECTS/src/Components/Random-Color-Generator/Random.jsx
@@ -2,16 +2,17 @@
 
 import React, { useState } from 'react'
 
-const [typeOfColor, setTypeOfColor] = useState('hex')
-const [color, setColor] = useState('#000000')
-
 const Random = () => {
+
+  const [typeOfColor, setTypeOfColor] = useState('hex')
+  const [color, setColor] = useState('#000000')
+
   return (
     <>
-      <div className={`w-[100vw] h-[100vh] bg-${color}`}>
-        <button>Create HEX Color</button>
-        <button>Create RGB Color</button>
-        <button>Create Random Color</button>
+      <div className='w-[100vw] h-[100vh] text-black font-bold block m-auto text-center space-x-10 space-y-5' style={{ backgroundColor: color }}>
+        <button className='bg-white px-5 py-1 rounded-md hover:brightness-150'>Create HEX Color</button>
+        <button className='bg-white px-5 py-1 rounded-md hover:brightness-150'>Create RGB Color</button>
+        <button className='bg-white px-5 py-1 rounded-md hover:brightness-150'>Create Random Color</button>
       </div>
     </>
   )
